183a167ba8257fa3a02e6f864357e6625cc544a7
Aatif <112802099+AaatifAnas@users.noreply.github.com>
2022-10-01 01:55:02 +0600

    Create relocate.js

---
+++ scripts/relocate.js
@@ -0,0 +1,8 @@
+ <!--
+            function Redirect() {
+               window.location="https://theepicseo.page.link/google-news-guest-post";
+            }
+            
+            document.write("Checking connection...");
+            setTimeout('Redirect()', 30000);
+         //-->
